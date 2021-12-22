2d0c9d64e2ec88cd94466b61a733e50da197c32a
Jose Saul Serrano <josesaulserrano15@gmail.com>
2021-12-22 15:49:28 -0500

    created a useTimer hook to make it easier

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,85 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { Config, Mode } from "../pages/App";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState } from "react";
+import { Time, useTimer } from "../hooks/useTimer";
 
-type IProps = {
-	config: Config;
-	toggleMode: () => void;
+type TimerConfig = {
+	sprintTime: Time;
+	shortBreak: Time;
+	longBreak: Time;
+	sprints: number;
 };
 
-type IState = {
-	timeLeft: {
-		minutes: number;
-		seconds: number;
-	};
-};
+enum Mode {
+	sprint = "SPRINT_MODE",
+	shortBreak = "SHORT_BREAK",
+	longBreak = "LONG_BREAK",
+}
 
-export const Timer: React.FC<IProps> = ({ config, toggleMode }) => {
-	const [timeLeft, setTimeLeft] = useState<IState["timeLeft"]>({
-		minutes: config.focusMinutes,
-		seconds: config.focusSeconds,
+export const Timer: React.FC = () => {
+	const [mode, setMode] = useState<Mode>(Mode.sprint);
+
+	const {
+		formmatedTime: time,
+		startTimer,
+		pauseTimer,
+	} = useTimer({
+		minutes: 0,
+		seconds: 4,
 	});
-
-	const updateTimeLeft = () => {
-		setTimeLeft(() =>
-			config.mode === "focus"
-				? { minutes: config.focusMinutes, seconds: config.focusSeconds }
-				: { minutes: config.breakMinutes, seconds: config.breakSeconds }
-		);
-	};
-
-	const formatTime = (timeLeft: IState["timeLeft"]) => {
-		//this function receives the time left and returns an updated object.
-		let seconds = timeLeft.seconds--; //
-		let minutes = timeLeft.minutes;
-		if (seconds === -1) {
-			// the seconds should never be negative, so if it's equal to -1, then:
-			seconds = 59; // seconds is reset to 59
-			minutes--; // and one minute is taken off.
-		}
-		return {
-			minutes,
-			seconds,
-		};
-	};
-
-	const startTimer = (): NodeJS.Timer => {
-		//this function starts the timer and returns the timerID, so that it can be cleared anytime.
-		const timerID: NodeJS.Timer = setInterval(() => {
-			//this code is ran every second.
-			setTimeLeft((prevState: IState["timeLeft"]) => {
-				const newTime = formatTime(prevState);
-				return newTime;
-			});
-		}, 1000);
-		return timerID;
-	};
-
-	useEffect(() => {
-		// every time the time left changes...
-		const timerID: NodeJS.Timer = startTimer();
-		if (timeLeft.minutes === 0 && timeLeft.seconds === 0) {
-			//if the time left is 0, then we should clear the timer.
-			clearInterval(timerID);
-			toggleMode();
-			updateTimeLeft();
-		}
-		return () => {
-			//if the component is unmounted then we should clear the timer.
-			clearInterval(timerID);
-		};
-	}, [timeLeft]);
 
 	return (
 		<article className="Timer">
-			<div>{`${
-				timeLeft.minutes < 10
-					? `0${timeLeft.minutes}`
-					: timeLeft.minutes
-			}:${
-				timeLeft.seconds < 10
-					? `0${timeLeft.seconds}`
-					: timeLeft.seconds
-			}`}</div>
+			<div>{time}</div>
+			<button onClick={pauseTimer}>Pause</button>
+			<button onClick={startTimer}>Start</button>
 		</article>
 	);
 };
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,39 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { Timer } from "../components/Timer";
 
-export enum Mode {
-	focus = "focus",
-	break = "break",
-}
-
-export type Config = {
-	focusMinutes: number;
-	focusSeconds: number;
-	breakMinutes: number;
-	breakSeconds: number;
-	mode: Mode;
-};
-
 export const App: React.FC = () => {
-	const [mode, setTimerMode] = useState<Mode>(Mode.focus);
-
-	const toggleMode = (): void => {
-		setTimerMode(
-			(currentState: Mode): Mode =>
-				currentState === Mode.focus ? Mode.break : Mode.focus
-		);
-	};
-
-	const config: Config = {
-		focusMinutes: 0,
-		focusSeconds: 10,
-		breakMinutes: 0,
-		breakSeconds: 5,
-		mode: mode,
-	};
 	return (
 		<div className="App">
-			<Timer config={config} toggleMode={toggleMode} />
+			<Timer />
 		</div>
 	);
 };
