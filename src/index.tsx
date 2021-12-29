0e70b0c651e50ed56102f38f35bfb8e495dfb268
Jose Saul Serrano <josesaulserrano15@gmail.com>
2021-12-29 11:30:05 -0500

    trying to restart the project...

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { TimerConfigProvider } from "./context/timerConfig";
 import { App } from "./pages/App";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<TimerConfigProvider>
-			<App />
-		</TimerConfigProvider>
+		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
