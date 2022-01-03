7aa0577693faa7ab5bafbc0bc7f21a27d3054f0c
Jose Saul Serrano <josesaulserrano15@gmail.com>
2022-01-03 12:12:05 -0500

    hotfix: disabled exhaustive deps for useEffect

--- src/hooks/usePomodoro.ts
+++ src/hooks/usePomodoro.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useRef, useState } from "react";
 import { Time, TimerConfigContext } from "../context/timerConfig";
 
